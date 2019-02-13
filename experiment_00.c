47859673a36101006557398c5245350402b75dd8
Riccardo Massidda <contact@rmassidda.it>
2019-02-13 22:03:13 +0100

    Example is now called experiment_00 and there's no lib folder anymore

---
+++ experiment_00.c
@@ -0,0 +1,17 @@
+#include "fileManager.h"
+
+struct filemanager *fm;  /*  fasta/fastq file maneger  */
+struct sequence_t *seq;
+
+int main(void){
+    char *filename = "data/chrY.fa";
+    fm = filemanager_init (filename);
+    seq = NULL;
+    while (seq == NULL) {
+      seq = filemanager_next_seq (fm, seq);
+      printf("%s",seq->sequence);
+    }
+
+    filemanager_destroy (fm);  /*  Releasefile managemant resources  */
+    return 0;
+}
