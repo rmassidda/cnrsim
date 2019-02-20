9b8c5a0c894d97fda1f6fc8d4f9c069498a8f9eb
Riccardo Massidda <contact@rmassidda.it>
2019-02-20 14:58:32 +0100

    Variator MULTIFASTA

--- variator.c
+++ variator.c
@@ -1,4 +1,5 @@
 #include <htslib/vcf.h>
+#include <htslib/synced_bcf_reader.h>
 #include <stdio.h>
 #include <time.h>
 #include <stdlib.h>
@@ -9,10 +10,9 @@
 
 #define ALL_N 2
 
-allele_t * allele_init(long int size){
-    allele_t * allele = malloc((sizeof(struct allele_t)));
+allele_t * allele_init(long int size, allele_t * allele){
     allele->buffer_size = floor( size * 1.5 );
-    allele->sequence = malloc((sizeof(char)) * allele->buffer_size );
+    allele->sequence = realloc( allele->sequence, (sizeof(char)) * allele->buffer_size );
     allele->pos = 0;
     allele->off = 0;
     return allele;
@@ -22,9 +22,10 @@ int main(int argc, char **argv){
     // FASTA
     struct filemanager *fm;
     struct sequence_t *seq;
+    char * label;
     long int ref_pos = 0;
     // VCF
-    htsFile * inf;
+    bcf_srs_t * sr;
     bcf_hdr_t * hdr;
     bcf1_t * line; 
     // Alleles
@@ -56,12 +57,29 @@ int main(int argc, char **argv){
     }
 
     // VCF file
-    inf = bcf_open (argv[1], "r" );
-    if ( inf == NULL ){
+    sr = bcf_sr_init ();
+    bcf_sr_set_opt ( sr, BCF_SR_REQUIRE_IDX );
+    if ( bcf_sr_add_reader ( sr, argv[1] ) != 1 ){
         exit ( EXIT_FAILURE );
     }
+
+    // File is not indexed
+    if ( sr->regions == NULL ){
+        int res = bcf_index_build ( argv[1], 4 );
+        if ( res == 0 ){
+            // File is now indexed
+            bcf_sr_remove_reader ( sr, 0 );
+            if ( bcf_sr_add_reader ( sr, argv[1] ) != 1 ){
+                exit ( EXIT_FAILURE );
+            }
+        }
+        else{
+            perror ( "File is not indexable.\n" );
+            exit ( EXIT_FAILURE );
+        }
+    }
     // VCF header
-    hdr = bcf_hdr_read(inf);
+    hdr = bcf_sr_get_header ( sr, 0 );
     // VCF line
     line = bcf_init();
 
@@ -70,9 +88,7 @@ int main(int argc, char **argv){
     if ( fm == NULL ){
         exit ( EXIT_FAILURE );
     }
-    // FASTA sequence
-    seq = filemanager_next_seq (fm, NULL);
-
+    
     // OUTPUT
     str = malloc( sizeof(char) * (strlen(argv[3]) + 20 ));
     for ( int i = 0; i < ALL_N; i++ ){
@@ -80,99 +96,119 @@ int main(int argc, char **argv){
         output[i] = fopen( str, "w+" );
     }
     
-    // Allocazione di una struttura per ogni allele
+    // FASTA sequence
+    seq = filemanager_next_seq (fm, NULL);
     for ( int i = 0; i < ALL_N; i++ ){
-        allele[i] = allele_init( seq->sequence_size );
+        allele[i] = malloc ( sizeof(struct allele_t) );
+        allele[i]->sequence = NULL;
     }
-
-    // Fino alla fine del VCF
-    while (bcf_read(inf, hdr, line) == 0){
-        // Lettura della linea
-        if (bcf_unpack( line, BCF_UN_STR) != 0){
-            perror("Unpack error");
-            exit(EXIT_FAILURE);
-        }
-        // Distanza tra la variazione ed il carattere ancora da leggere
-        distance = line->pos - ref_pos;
-        // Per ogni allele
+    while ( seq != NULL ){
+        // Allocazione di una struttura per ogni allele
         for ( int i = 0; i < ALL_N; i++ ){
-            if ( distance >= 0 ){
-                // Copia in blocco [ref_pos, var_pos)
-                // dal reference all'allele
-                memcpy( 
-                        &allele[i]->sequence[allele[i]->pos], 
-                        &seq->sequence[ref_pos] , 
-                        distance
-                        );
-            }
-            // La posizione sull'allele varia al netto del segno della distanza
-            allele[i]->pos += distance;    
-            // Test
-            // La posizione del puntatore sull'allele è coerente?
-            assert ( line->pos == allele[i]->pos + allele[i]->off );
-            // Il reference è coerente con quello descritto dalla variazione?
-            ref_check = realloc (ref_check, sizeof(char) * ( strlen( line->d.allele[0] ) + 1 ) );
-            sprintf ( ref_check, "%.*s", (int) strlen ( line->d.allele[0] ), &seq->sequence[ line->pos ] ); 
-            assert ( strcmp ( ref_check, line->d.allele[0] ) == 0 );
-            // Definito dallo standard
-            af_ret = bcf_get_info_float( hdr, line, "AF", af, &af_size );
-            // Usato da dbSNP
-            freq_ret = bcf_get_info_string( hdr, line, "FREQ", &freq, &freq_size );
-            // Controllo dei risultati
-            if ( af_ret >= 0  ){
-                p = parse_af( line->n_allele, af, p );
-            }
-            else if (freq_ret >= 0){
-                p = parse_db_snp_freq( line->n_allele, freq, p );
-            }
-            else{
-                p = linear( line->n_allele, p );
-            }
-            
-            // Estrazione dell'allele
-            outcome = (double)rand() / RAND_MAX;
-            threshold = 0;
-            for (int i = 0; i < line->n_allele; i++){
-                if ( threshold <= outcome && outcome < threshold + p[i] ){
-                    subseq = line->d.allele[i];
-                    break;
-                }
-                else{
-                    threshold += p[i];
-                }
-            }
-            // Inserimento dei caratteri
-            memcpy(
-                    &allele[i]->sequence[allele[i]->pos],
-                    subseq,
-                    strlen(subseq)
-                  );
-            // Aggiornamento dell'offset
-            // TODO: si può recuperare questa informazione dal VCF?
-            allele[i]->off += strlen(line->d.allele[0]) - strlen(subseq);
-            allele[i]->pos += strlen(subseq);
+            allele[i] = allele_init( seq->sequence_size, allele[i]);
         }
-        // Aggiornamento della posizione sul reference
-        ref_pos += (distance + strlen(line->d.allele[0]));
+        label = strtok ( seq->label, " " );
+        // Seek on VCF file
+        if ( bcf_sr_seek ( sr, label, 0 ) == 0 ){
+            // Fino alla fine del VCF
+            while ( bcf_sr_next_line ( sr ) ){
+                line = bcf_sr_get_line ( sr, 0 );
+                // Lettura della linea
+                if (bcf_unpack( line, BCF_UN_STR) != 0){
+                    perror("Unpack error");
+                    exit(EXIT_FAILURE);
+                }
+                // Distanza tra la variazione ed il carattere ancora da leggere
+                distance = line->pos - ref_pos;
+                // Per ogni allele
+                for ( int i = 0; i < ALL_N; i++ ){
+                    if ( distance >= 0 ){
+                        // Copia in blocco [ref_pos, var_pos)
+                        // dal reference all'allele
+                        memcpy( 
+                                &allele[i]->sequence[allele[i]->pos], 
+                                &seq->sequence[ref_pos] , 
+                                distance
+                                );
+                    }
+                    // La posizione sull'allele varia al netto del segno della distanza
+                    allele[i]->pos += distance;    
+                    // Test
+                    // La posizione del puntatore sull'allele è coerente?
+                    assert ( line->pos == allele[i]->pos + allele[i]->off );
+                    // Il reference è coerente con quello descritto dalla variazione?
+                    ref_check = realloc (ref_check, sizeof(char) * ( strlen( line->d.allele[0] ) + 1 ) );
+                    sprintf ( ref_check, "%.*s", (int) strlen ( line->d.allele[0] ), &seq->sequence[ line->pos ] ); 
+                    assert ( strcmp ( ref_check, line->d.allele[0] ) == 0 );
+                    // Definito dallo standard
+                    af_ret = bcf_get_info_float( hdr, line, "AF", af, &af_size );
+                    // Usato da dbSNP
+                    freq_ret = bcf_get_info_string( hdr, line, "FREQ", &freq, &freq_size );
+                    // Controllo dei risultati
+                    if ( af_ret >= 0  ){
+                        p = parse_af( line->n_allele, af, p );
+                    }
+                    else if (freq_ret >= 0){
+                        p = parse_db_snp_freq( line->n_allele, freq, p );
+                    }
+                    else{
+                        p = linear( line->n_allele, p );
+                    }
+                    
+                    // Estrazione dell'allele
+                    outcome = (double)rand() / RAND_MAX;
+                    threshold = 0;
+                    for (int i = 0; i < line->n_allele; i++){
+                        if ( threshold <= outcome && outcome < threshold + p[i] ){
+                            subseq = line->d.allele[i];
+                            break;
+                        }
+                        else{
+                            threshold += p[i];
+                        }
+                    }
+                    // Inserimento dei caratteri
+                    memcpy(
+                            &allele[i]->sequence[allele[i]->pos],
+                            subseq,
+                            strlen(subseq)
+                          );
+                    // Aggiornamento dell'offset
+                    // TODO: si può recuperare questa informazione dal VCF?
+                    allele[i]->off += strlen(line->d.allele[0]) - strlen(subseq);
+                    allele[i]->pos += strlen(subseq);
+                }
+                // Aggiornamento della posizione sul reference
+                ref_pos += (distance + strlen(line->d.allele[0]));
+            }
+            // Inserimento della parte finale del reference
+            for ( int i = 0; i < ALL_N; i++ ){
+                distance = seq->sequence_size - ref_pos;
+                memcpy(
+                        &allele[i]->sequence[allele[i]->pos],
+                        &seq->sequence[ref_pos], 
+                        distance
+                      );
+                // Aggiornamento della posizione
+                allele[i]->pos += distance;
+                // Carattere di fine stringa
+                allele[i]->pos = 0;
+            }
+            // Write of the sequence on file
+            for ( int i = 0; i < ALL_N; i++ ){
+                fprintf ( output[i], ">%s\n", label );
+                fprintf ( output[i], allele[i]->sequence );
+            }
+        }
+        else{
+            fprintf ( stderr, "Sequence %s not found in VCF\n", label );
+        }
+        seq = filemanager_next_seq (fm, NULL);
     }
 
-    // Inserimento della parte finale del reference
-    for ( int i = 0; i < ALL_N; i++ ){
-        distance = seq->sequence_size - ref_pos;
-        memcpy(
-                &allele[i]->sequence[allele[i]->pos],
-                &seq->sequence[ref_pos], 
-                distance
-              );
-        // Aggiornamento della posizione
-        allele[i]->pos += distance;
-        // Carattere di fine stringa
-        allele[i]->pos = 0;
-    }
 
-    // Scrittura su file
+    // Cleanup
     for ( int i = 0; i < ALL_N; i++ ){
-        fprintf( output[i], allele[i]->sequence );
         free( allele[i]->sequence );
         free( allele[i] );
         fclose( output[i] );
@@ -180,7 +216,6 @@ int main(int argc, char **argv){
     free ( p );
     bcf_destroy( line );
     bcf_hdr_destroy( hdr );
-    bcf_close( inf );
     filemanager_destroy( fm );
     exit(EXIT_SUCCESS);
 }
